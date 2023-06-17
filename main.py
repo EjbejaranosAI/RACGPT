d4e428b8eb1413ad2123cca274ed7330d0caffb0
Danny Stevens <Dstrasla@gmail.com>
2023-06-16 19:02:03 -0500

    Changes by edisonjairbejaranosepulveda

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+
+Import extern libraries
+Import own libraries
+
+Call clases or functions to construct the streamlit fronted
+Call clases to instanced the LLMs model
+Deploy and inference
+
+"""
