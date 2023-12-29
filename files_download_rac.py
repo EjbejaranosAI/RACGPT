7c82ff51d0681f1f91dec1dc2539664b54f14e57
ejbejaranos <ejbejaranos@gmail.com>
2023-12-29 12:20:37 +0100

    fix(files_download_rac.py): fix incorrect variable name 'url' to 'target_url' for better clarity feat(files_download_rac.py): add logging configuration and logging statements for better visibility and error handling feat(files_download_rac.py): add function to read config file and handle errors when reading the file feat(files_download_rac.py): add function to create directory if it does not exist feat(files_download_rac.py): add function to get PDF links from a webpage feat(files_download_rac.py): add function to download a PDF from a given URL and save it to a specified path feat(files_download_rac.py): add function to fetch response from a given URL and handle errors feat(files_download_rac.py): add function to download a specified number of PDFs from a target URL and save them to a specified storage path feat(files_download_rac.py): add function to get PDF data by reading configuration file and downloading PDFs based on the configuration feat(files_download_rac.py): add main block to call get_pdf_data function when the script is run directly

--- files_download_rac.py
+++ files_download_rac.py
@@ -3,50 +3,89 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import logging
 
-# Path to your config.json file
-config_file_path = './config.json'
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Read the config.json file
-with open(config_file_path, 'r') as file:
-    config = json.load(file)
+def read_config(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        logger.error(f"Error reading config file: {e}")
+        return None
 
-# Accessing variables from the config file
-name = config['name']
-url = config['url']
-data_location = config['data_location']
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
-def get_rac_pdfs(url_link, data_location, max_pdfs=None):
-    # Create directory if it does not exist
-    if not os.path.exists(data_location):
-        os.mkdir(data_location)
 
-    response = requests.get(url_link)
-    soup = BeautifulSoup(response.text, "html.parser")
+def get_pdf_links(url_link, soup):
+    pdf_links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and '.pdf' in href:
+            full_url = urljoin(url_link, href)
+            pdf_links.append(full_url)
+    return pdf_links
 
-    # Find all links on the page
-    links = soup.find_all('a')
+def download_pdf(pdf_url, save_path):
+    if os.path.exists(save_path):
+        logger.info(f"{save_path} already exists, skipping.")
+        return
+    try:
+        response = requests.get(pdf_url, stream=True)
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
+        logger.info(f"Downloaded {save_path}")
+    except requests.RequestException as e:
+        logger.error(f"Error downloading {pdf_url}: {e}")
 
-    # Filter out the links to PDFs
-    pdf_links = [urljoin(url_link, link.get('href')) for link in links if link.get('href') and '.pdf' in link.get('href')]
+def get_response(url_link):
+    try:
+        return requests.get(url_link)
+    except requests.RequestException as e:
+        logger.error(f"Error fetching {url_link}: {e}")
+        return None
 
-    # Determine the number of PDFs to download
-    num_pdfs_to_download = len(pdf_links) if max_pdfs is None else min(len(pdf_links), max_pdfs)
+def get_rac_pdfs(target_url, storage_path, max_download_limit=None):
+    create_directory(storage_path)
+    response = get_response(target_url)
+    if response:
+        soup = BeautifulSoup(response.text, "html.parser")
+        pdf_links = get_pdf_links(target_url, soup)
+        num_pdfs_to_download = len(pdf_links) if max_download_limit is None else min(len(pdf_links), max_download_limit)
 
-    for i in range(num_pdfs_to_download):
-        pdf_url = pdf_links[i]
-        response = requests.get(pdf_url)
-        pdf_name = os.path.join(data_location, f'document_{i}.pdf')
+        for i, pdf_url in enumerate(pdf_links[:num_pdfs_to_download]):
+            pdf_name = os.path.join(storage_path, f'document_{i}.pdf')
+            download_pdf(pdf_url, pdf_name)
+    else:
+        logger.error("Failed to retrieve the webpage.")
 
-        with open(pdf_name, 'wb') as file:
-            file.write(response.content)
-        print(f'Downloaded {pdf_name}')
+def get_pdf_data():
+    config = read_config('./config.json')
+    if not config:
+        logger.error("Failed to read configuration, exiting.")
+        return
 
-# Use the variables in your project
-print(name)
-print(url)
-print(data_location)
+    # Accessing new variables from config
+    project_name = config['project_name']
+    download_settings = config['pdf_download_settings']
+    targetUrl = download_settings['target_url']
+    storagePath = download_settings['storage_path']
+    maxDownloadLimit = download_settings.get('max_download_limit')  # This will be None if not set
 
-# Example usage: Download up to 3 PDFs
-get_rac_pdfs(url, data_location, max_pdfs=3)
+    logger.info(f"Project Name: {project_name}")
+    logger.info(f"Starting PDF download from: {targetUrl}")
+    logger.info(f"Saving to: {storagePath}")
+    logger.info(f"Maximum PDFs to download: {'All' if maxDownloadLimit is None else maxDownloadLimit}")
 
+    # Download PDFs
+    get_rac_pdfs(targetUrl, storagePath, maxDownloadLimit)
+
+if __name__ == "__main__":
+    get_pdf_data()
